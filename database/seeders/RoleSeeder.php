2502004272758f34eaabee0f2d3ed811eb18b33a
ipincamp <support@nur-arifin.my.id>
2025-06-28 00:35:00 +0700

    feat: Add RoleEnum and RoleSeeder for role management in database seeding

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleEnum;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            'name' => RoleEnum::ADMIN->value,
+        ]);
+        Role::create([
+            'name' => RoleEnum::USER->value,
+        ]);
+    }
+}
