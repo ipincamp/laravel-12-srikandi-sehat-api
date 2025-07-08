dddb0fd7a3a205e81e1eec4e7abb4dd7e087b450
ipincamp <support@nur-arifin.my.id>
2025-07-08 20:01:31 +0700

    feat: Add seeders for Province and Regency with data for improved database seeding

--- database/seeders/DistrictSeeder.php
+++ database/seeders/DistrictSeeder.php
@@ -3,50 +3,61 @@
 namespace Database\Seeders;
 
 use App\Models\District;
+use App\Models\Regency; // <-- Tambahkan ini
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // <-- Tambahkan ini
 
 class DistrictSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
+        $banyumasRegency = Regency::where('code', '3302')->first();
+
+        if (!$banyumasRegency) {
+            $this->command->error('Regency with code 3302 (BANYUMAS) not found. Please run RegencySeeder first.');
+            return;
+        }
+
         $districts = [
-            ['id' => 10, 'name' => 'KECAMATAN LUMBIR'],
-            ['id' => 20, 'name' => 'KECAMATAN WANGON'],
-            ['id' => 30, 'name' => 'KECAMATAN JATILAWANG'],
-            ['id' => 40, 'name' => 'KECAMATAN RAWALO'],
-            ['id' => 50, 'name' => 'KECAMATAN KEBASEN'],
-            ['id' => 60, 'name' => 'KECAMATAN KEMRANJEN'],
-            ['id' => 70, 'name' => 'KECAMATAN SUMPIUH'],
-            ['id' => 80, 'name' => 'KECAMATAN TAMBAK'],
-            ['id' => 90, 'name' => 'KECAMATAN SOMAGEDE'],
-            ['id' => 100, 'name' => 'KECAMATAN KALIBAGOR'],
-            ['id' => 110, 'name' => 'KECAMATAN BANYUMAS'],
-            ['id' => 120, 'name' => 'KECAMATAN PATIKRAJA'],
-            ['id' => 130, 'name' => 'KECAMATAN PURWOJATI'],
-            ['id' => 140, 'name' => 'KECAMATAN AJIBARANG'],
-            ['id' => 150, 'name' => 'KECAMATAN GUMELAR'],
-            ['id' => 160, 'name' => 'KECAMATAN PEKUNCEN'],
-            ['id' => 170, 'name' => 'KECAMATAN CILONGOK'],
-            ['id' => 180, 'name' => 'KECAMATAN KARANGLEWAS'],
-            ['id' => 190, 'name' => 'KECAMATAN KEDUNG BANTENG'],
-            ['id' => 200, 'name' => 'KECAMATAN BATURRADEN'],
-            ['id' => 210, 'name' => 'KECAMATAN SUMBANG'],
-            ['id' => 220, 'name' => 'KECAMATAN KEMBARAN'],
-            ['id' => 230, 'name' => 'KECAMATAN SOKARAJA'],
-            ['id' => 710, 'name' => 'KECAMATAN PURWOKERTO SELATAN'],
-            ['id' => 720, 'name' => 'KECAMATAN PURWOKERTO BARAT'],
-            ['id' => 730, 'name' => 'KECAMATAN PURWOKERTO TIMUR'],
-            ['id' => 740, 'name' => 'KECAMATAN PURWOKERTO UTARA'],
+            ['code' => '3302010', 'name' => 'LUMBIR'],
+            ['code' => '3302020', 'name' => 'WANGON'],
+            ['code' => '3302030', 'name' => 'JATILAWANG'],
+            ['code' => '3302040', 'name' => 'RAWALO'],
+            ['code' => '3302050', 'name' => 'KEBASEN'],
+            ['code' => '3302060', 'name' => 'KEMRANJEN'],
+            ['code' => '3302070', 'name' => 'SUMPIUH'],
+            ['code' => '3302080', 'name' => 'TAMBAK'],
+            ['code' => '3302090', 'name' => 'SOMAGEDE'],
+            ['code' => '3302100', 'name' => 'KALIBAGOR'],
+            ['code' => '3302110', 'name' => 'BANYUMAS'],
+            ['code' => '3302120', 'name' => 'PATIKRAJA'],
+            ['code' => '3302130', 'name' => 'PURWOJATI'],
+            ['code' => '3302140', 'name' => 'AJIBARANG'],
+            ['code' => '3302150', 'name' => 'GUMELAR'],
+            ['code' => '3302160', 'name' => 'PEKUNCEN'],
+            ['code' => '3302170', 'name' => 'CILONGOK'],
+            ['code' => '3302180', 'name' => 'KARANGLEWAS'],
+            ['code' => '3302190', 'name' => 'KEDUNG BANTENG'],
+            ['code' => '3302200', 'name' => 'BATURRADEN'],
+            ['code' => '3302210', 'name' => 'SUMBANG'],
+            ['code' => '3302220', 'name' => 'KEMBARAN'],
+            ['code' => '3302230', 'name' => 'SOKARAJA'],
+            ['code' => '3302710', 'name' => 'PURWOKERTO SELATAN'],
+            ['code' => '3302720', 'name' => 'PURWOKERTO BARAT'],
+            ['code' => '3302730', 'name' => 'PURWOKERTO TIMUR'],
+            ['code' => '3302740', 'name' => 'PURWOKERTO UTARA'],
         ];
 
         $now = now();
-        $districts = array_map(function ($district) use ($now) {
-            return array_merge($district, [
-                'created_at' => $now,
-                'updated_at' => $now,
-            ]);
-        }, $districts);
+        foreach ($districts as &$district) {
+            $district['regency_id'] = $banyumasRegency->id;
+            $district['created_at'] = $now;
+            $district['updated_at'] = $now;
+        }
 
-        District::insert($districts);
+        DB::table('districts')->insert($districts);
     }
 }
