25ad599c5101c922272f5884c447cae647291c45
ipincamp <support@nur-arifin.my.id>
2025-07-11 22:54:00 +0700

    feat: Update symptom categories to use proper casing for consistency

--- database/migrations/2025_07_11_213945_create_symptoms_table.php
+++ database/migrations/2025_07_11_213945_create_symptoms_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->enum('category', [
-                'mood',
-                'physical',
-                'other',
+                'Mood',
+                'Fisik',
+                'Lainnya',
                 /*
                 'dismenorea',
                 'suasana-hati',
